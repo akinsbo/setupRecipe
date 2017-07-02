d5f51ea5eff1b442e6f6b4362e46419de90ba905
Olaolu Akinsete <akinsbo@gmail.com>
2017-07-02 17:02:48 +0100

    added reactjs for frontend

--- utils/shell-scripts/installation-scripts/create-react-app-install.sh
+++ utils/shell-scripts/installation-scripts/create-react-app-install.sh
@@ -4,9 +4,18 @@ DEV_FRONTEND_DIR = "dev/frontend_dev/"
 echo "**Welcome to project b**"
 echo "installing nodejs"
 sudo apt-get update
-sudo apt-get install nodejs
+sudo apt-get install -g nodejs
+echo "installing to node into /usr/bin/env to avoid such error"
+sudo apt-get update
+sudo apt-get install nodejs-legacy
 echo "installing npm"
+sudo apt-get update
 sudo apt-get install npm
+echo "upgrade node to stable version"
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
 echo "installing reactjs"
-sudo npm install -g create-react-app
+sudo apt-get update
+sudo npm install create-react-app
 
