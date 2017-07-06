342a7cd06279f7d891a5543f33548dfeca1dc7da
Olaolu Akinsete <akinsbo@gmail.com>
2017-07-06 13:47:59 +0100

    added docker provate registry script and make recipe

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 DEV_DIR = dev
 DEV_FRONTEND_DIR = dev/frontend_dev/
-UTIL_SCRIPTS_DIR = utils/shell-scripts
+UTIL_SCRIPTS_DIR = utils/
 
 help:
 	@echo	""
@@ -11,6 +11,9 @@ help:
 	create-react-app (with nodejs and npm), jenkins. \
 	Best for a virgin Linux OS setup"
 	@echo "	run-frontend - builds and runs create-react-app and docker"
+	@echo "view-images-in-registry - list images in docker repository \
+	and their tags where the bash script is customized to do so\
+	or use the url to"
 # install all you need on fresh linux system
 #
 install-all:
@@ -28,3 +31,11 @@ run-frontend:
 stop-frontend:
 	@echo "	stopping docker container"
 	cd $(DEV_FRONTEND_DIR)&& $(MAKE) docker-stop
+
+pull-image-to-docker-private-reg:
+	@echo " pulling image to private repository"
+	cd $(UTIL_SCRIPTS_DIR)&& $(MAKE) pull-image-to-docker-private-reg
+
+view-images-in-registry:
+	@echo " listing images in docker private repository"
+	cd $(UTIL_SCRIPTS_DIR)&& $(MAKE) view-images-in-registry
--- utils/shell-scripts/setups/docker-registry-setup.sh
+++ utils/shell-scripts/setups/docker-registry-setup.sh
@@ -1,17 +1,7 @@
-#!/bin/dash
-UBUNTU=ubuntu:16.04
-IMAGE=$UBUNTU
+REGISTRY_NAME=registry
+TAG_NAME=2
 echo "use registry image to deploy an container call registry"
-sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
-echo "pulling the image to use"
-sudo docker pull $IMAGE
-echo "taging the image as localhost:5000/my-$IMAGE"
-sudo docker tag $IMAGE localhost:5000/my-$IMAGE
-echo "pushing the image to the local registry runnung at localhost:5000"
-sudo docker push localhost:5000/my-$IMAGE
-echo "removing locally cached Image"
-sudo docker image remove $IMAGE
-sudo docker image remove localhost:5000/my-$IMAGE
+echo "sudo docker run -d -p 5000:5000 --restart=always --name registry $REGISTRY_NAME:$TAG_NAME"
 echo "setting the registry to start automatically"
 echo "mount the images in the mnt/registry registry docker folder to host's var/lib/registry folder"
 sudo docker run -d \
@@ -19,4 +9,4 @@ sudo docker run -d \
   --restart=always \
   --name registry \
   -v /mnt/registry:/var/lib/registry \
-  registry:2
+  $REGISTRY_NAME:$TAG_NAME
